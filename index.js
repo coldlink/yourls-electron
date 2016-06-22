552b29bffb187070b31e3bf537c5f198f92f0c2c
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2016-06-22 17:11:17 +0100

    overwrite default listeners to open window - use context menus instead, add messages to context menu, add quit to context menu, add dynamic context menu change, add url validation on quick shorten,

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const {
 let settings;
 
 let mb = menubar({
-	'show-on-right-click': true,
 	'tooltip': 'YOURLS Electron'
 });
 
@@ -26,7 +25,9 @@ mb.on('ready', function ready() {
 	let contextMenu = [{
 		label: 'Shorten from clipboard',
 		accelerator: 'CmdOrCtrl+Alt+S',
-		click: shorten
+		click: shortenClipboard
+	}, {
+		type: 'separator'
 	}, {
 		label: 'Open',
 		click: function() {
@@ -40,30 +41,48 @@ mb.on('ready', function ready() {
 				mb.window.webContents.send('redirect', 'settings');
 			});
 		}
+	}, {
+		type: 'separator'
+	}, {
+		label: 'Message: None',
+		enabled: false
+	}, {
+		type: 'separator'
+	}, {
+		label: 'Quit YOURLS Electron',
+		click: function () {
+			mb.app.quit();
+		}
 	}];
-	updateMenuBar();
 
 	/*GLOBAL SHORTCUTS*/
-	const ret = globalShortcut.register('CmdOrCtrl+Alt+S', shorten);
+	const ret = globalShortcut.register('CmdOrCtrl+Alt+S', shortenClipboard);
 
-	/*MESSAGE LISTENERS*/
-	ipcMain.on('settings', function(e, args) {
+	/*LISTENERS*/
+	ipcMain.on('settings', (e, args) => {
 		saveSettings(args);
 	});
 
+	//remove default open window on click
+	mb.tray.removeAllListeners();
+	mb.tray.on('click', showContextMenu);
+	mb.tray.on('right-click', showContextMenu);
+	mb.tray.on('double-click', function () {
+		mb.showWindow();
+	});
+
 	/*METHODS*/
-	function updateMenuBar() {
-		mb.tray.setContextMenu(Menu.buildFromTemplate(contextMenu));
+	function showContextMenu() {
+		mb.tray.popUpContextMenu(Menu.buildFromTemplate(contextMenu));
 	}
 
-	function shorten(e, longUrl) {
-		let req = settings.apiUrl + '/yourls-api.php?format=json&action=shorturl&signature=' + settings.token + '&url=';
-
-		if (longUrl) {
-			req += longUrl;
-		} else {
-			req += clipboard.readText();
+	function shortenClipboard(e) {
+		if (!validateUrl(clipboard.readText())) {
+			contextMenu[5].message = 'Message: Clipboard text does not look like a valid url!';
+			return;
 		}
+
+		let req = settings.apiUrl + '/yourls-api.php?format=json&action=shorturl&signature=' + settings.token + '&url=' + clipboard.readText();
 
 		console.log(req);
 		// request.get(req, function(err, response, body) {
@@ -80,10 +99,14 @@ mb.on('ready', function ready() {
 			if (err) throw err;
 		});
 	}
+
+	function validateUrl(value) {
+		return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+	}
 });
 
-mb.on('after-create-window', function() {
-	mb.window.openDevTools();
+mb.on('after-create-window', () => {
+	// mb.window.openDevTools();
 	mb.window.webContents.on('did-finish-load', () => {
 		mb.window.webContents.send('settings', settings);
 	});
